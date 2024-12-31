9cb8a399fbd8d99bdac24ae496d57bd3be783112
Josumaru <jokosupriyanto1712@gmail.com>
2024-12-31 22:25:03 +0700

    feat(testimonials): create testimonials section

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,12 +2,14 @@ import { NextPage } from "next";
 import Header from "@/components/home/header/header";
 import { LangProps } from "@/types/lang";
 import Services from "@/components/home/services/services";
+import Testimonials from "@/components/home/testimonials/testimonials";
 
 const Page: NextPage<LangProps> = async ({ params }) => {
   return (
     <div>
       <Header params={params} />
       <Services params={params} />
+      <Testimonials params={params}/>
     </div>
   );
 };
