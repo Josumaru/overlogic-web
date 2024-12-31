64fb123e3fde77cb04f721a347066287da711f5d
Josumaru <jokosupriyanto1712@gmail.com>
2024-12-31 22:42:25 +0700

    refactor(services): removed frammer animation on bento grid

--- src/components/home/services/services.tsx
+++ src/components/home/services/services.tsx
@@ -52,31 +52,32 @@ const Services: NextPage<LangProps> = async ({ params }) => {
       cta: "Learn more",
       className: "col-span-3 lg:col-span-1",
       background: (
-        <Marquee
-          pauseOnHover
-          className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
-        >
-          {files.map((f, idx) => (
-            <figure
-              key={idx}
-              className={cn(
-                "relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
-                "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-                "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-                "transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none"
-              )}
-            >
-              <div className="flex flex-row items-center gap-2">
-                <div className="flex flex-col">
-                  <figcaption className="text-sm font-medium dark:text-white ">
-                    {f.name}
-                  </figcaption>
-                </div>
-              </div>
-              <blockquote className="mt-2 text-xs">{f.body}</blockquote>
-            </figure>
-          ))}
-        </Marquee>
+        <div></div>
+        // <Marquee
+        //   pauseOnHover
+        //   className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
+        // >
+        //   {files.map((f, idx) => (
+        //     <figure
+        //       key={idx}
+        //       className={cn(
+        //         "relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
+        //         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        //         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        //         "transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none"
+        //       )}
+        //     >
+        //       <div className="flex flex-row items-center gap-2">
+        //         <div className="flex flex-col">
+        //           <figcaption className="text-sm font-medium dark:text-white ">
+        //             {f.name}
+        //           </figcaption>
+        //         </div>
+        //       </div>
+        //       <blockquote className="mt-2 text-xs">{f.body}</blockquote>
+        //     </figure>
+        //   ))}
+        // </Marquee>
       ),
     },
     {
@@ -86,7 +87,11 @@ const Services: NextPage<LangProps> = async ({ params }) => {
       href: "#",
       cta: "Learn more",
       className: "col-span-3 lg:col-span-2",
-      background: <RetroGrid />,
+      background: (
+        <div>
+          {/* <RetroGrid /> */}
+        </div>
+      ),
     },
     {
       Icon: GlobeIcon,
@@ -97,7 +102,7 @@ const Services: NextPage<LangProps> = async ({ params }) => {
       className: "col-span-3 lg:col-span-2",
       background: (
         <div>
-          <Globe />
+          {/* <Globe /> */}
         </div>
       ),
     },
@@ -109,11 +114,13 @@ const Services: NextPage<LangProps> = async ({ params }) => {
       href: "#",
       cta: "Learn more",
       background: (
-        <Calendar
-          mode="single"
-          selected={new Date(2022, 4, 11, 0, 0, 0)}
-          className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-        />
+        <div>
+          {/* <Calendar
+            mode="single"
+            selected={new Date(2022, 4, 11, 0, 0, 0)}
+            className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
+          /> */}
+        </div>
       ),
     },
   ];
