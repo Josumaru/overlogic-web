a36f77d9f4a95c12ab8ca2b53657266701929648
mfl4 <mhdfarhanl6267@gmail.com>
2025-01-07 05:06:09 +0700

    refactor(home): call the project and team components

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -4,14 +4,18 @@ import { LangProps } from "@/types/lang";
 import Services from "@/components/home/services/services";
 import Testimonials from "@/components/home/testimonials/testimonials";
 import Faq from "@/components/home/faq/faq";
+import Project from "@/components/home/project/project";
+import Team from "@/components/home/team/team";
 
 const Page: NextPage<LangProps> = async ({ params }) => {
   return (
     <div>
-      <Header params={params} />
-      <Services params={params} />
-      <Testimonials params={params}/>
-      <Faq params={params}/>
+        <Header params={params} />
+        <Services params={params} />
+        <Project params={params}/>
+        <Team params={params}/>
+        <Testimonials params={params}/>
+        <Faq params={params}/>
     </div>
   );
 };
